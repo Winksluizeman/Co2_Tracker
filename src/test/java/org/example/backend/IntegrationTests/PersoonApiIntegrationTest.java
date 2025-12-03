fa0cbb648e4a3b2f2c3cefb63a7446368a2cec27
WinkSluizeman53 <563017@student.fontys.nl>
2025-12-03 15:53:05 +0100

    Added fixes for ci pipeline

--- src/test/java/org/example/backend/IntegrationTests/PersoonApiIntegrationTest.java
+++ src/test/java/org/example/backend/IntegrationTests/PersoonApiIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,7 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@Transactional
 class PersoonApiIntegrationTest {
+
 
     @Autowired
     private TestRestTemplate restTemplate;
