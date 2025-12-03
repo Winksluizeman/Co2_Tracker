a8fde1c38b4c4ad53c124d255ef7cbb279facd60
WinkSluizeman53 <563017@student.fontys.nl>
2025-12-03 15:59:06 +0100

    Added fixes for ci pipeline

--- src/test/java/org/example/backend/DalTests/PersoonDalTests.java
+++ src/test/java/org/example/backend/DalTests/PersoonDalTests.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class PersooonDalTests {
 
     @Autowired
