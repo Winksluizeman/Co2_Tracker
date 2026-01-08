3f59af3b1656093f0aa0694de5ea5f5fbfb0c42d
WinkSluizeman53 <563017@student.fontys.nl>
2026-01-08 21:01:17 +0100

    Bug fix, last push before integraion test

--- src/main/java/org/example/backend/Application/Port/ICreateAccountRepo.java
+++ src/main/java/org/example/backend/Application/Port/ICreateAccountRepo.java
@@ -1,9 +1,7 @@
 package org.example.backend.Application.Port;
 
 import org.example.backend.Domain.CreateAccountModel;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ICreateAccountRepo {
 
     CreateAccountModel createAccount(CreateAccountModel createAccountModel);
