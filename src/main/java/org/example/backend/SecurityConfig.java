51efdfaa9fd21ab9a94e8b03867c97a1f5a01a28
WinkSluizeman53 <563017@student.fontys.nl>
2026-01-16 13:10:02 +0100

    Alle testen zijn weer werkend

--- src/main/java/org/example/backend/SecurityConfig.java
+++ src/main/java/org/example/backend/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
